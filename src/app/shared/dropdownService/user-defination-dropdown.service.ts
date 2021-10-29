da3fa803b8e0bdb248a3718b2f1a81e2cccd3d09
NITIN S. KAMBALE <nitin.kambale@erpcompserv.local>
2021-10-29 11:37:36 +0530

    manasi first commit

---
+++ src/app/shared/dropdownService/user-defination-dropdown.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { IOption } from 'ng-select';
+
+
+@Injectable()
+export class UserdefinationServiceD{
+    public static readonly PLAYER_ONE: Array<IOption> = [
+        { value: '0', label: 'Super Administrator' },
+        { value: '1', label: 'Administrator' },
+        { value: '2', label: 'Sub Administrator' },
+        { value: '3', label: 'Manager' },
+        { value: '4', label: 'Sub Manager' },
+        { value: '5', label: 'Clerk' },
+        { value: '6', label: 'Cashier' },
+        { value: '7', label: 'Guest' }
+    ];
+
+    getCharacters(): Array<IOption> {
+        return this.cloneOptions(UserdefinationServiceD.PLAYER_ONE);
+    }
+    loadCharacters(): Observable<Array<IOption>> {
+        return this.loadOptions(UserdefinationServiceD.PLAYER_ONE);
+    }
+
+    getCharactersWithDisabled(): Array<IOption> {
+        const characters: Array<IOption> = this.cloneOptions(UserdefinationServiceD.PLAYER_ONE);
+        characters[1].disabled = true;
+        characters[4].disabled = true;
+        return characters;
+    }
+
+
+
+    private loadOptions(options: Array<IOption>): Observable<Array<IOption>> {
+        return new Observable((obs) => {
+            setTimeout(() => {
+                obs.next(this.cloneOptions(options));
+                obs.complete();
+            }, 5000);
+        });
+    }
+
+    private cloneOptions(options: Array<IOption>): Array<IOption> {
+        return options.map(option => ({ value: option.value, label: option.label }));
+    }
+}
