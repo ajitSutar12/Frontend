f2a777b2d3df553c86111428c9d6d960ee0191ea
Shubhangi <shubhangi.nagare@compservtech.com>
2022-08-22 13:43:11 +0530

    testing changes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,8 @@ export const environment = {
   // base_url: 'http://139.59.86.29:4000'  //CBS regular build
   // base_url: 'http://139.59.86.29:7276'   //CBS test server
   // base_url: 'http://139.59.63.215:7276',  //CBS  BHAIRAVNATH server
-  base_url: 'http://localhost:3000',
+  base_url: 'http://localhost:7276',
+  // base_url: 'http://localhost:3000',
   // base_url: 'http://localhost:4000',
   // base_url: 'http://192.168.1.154:4000'  //sudam sir PC
   // report_url: 'http://139.59.63.215/bh_phpjasperxml-master/examples' //server bhairavnath report folder path
