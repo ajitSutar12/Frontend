ff26d69ed50c15b3bc1e7cdef17b2c27fc7e1b85
Shubhangi <shubhangi.nagare@compservtech.com>
2022-09-05 14:18:35 +0530

    daily Day book

--- src/app/theme/reports/daily-reports/t-form-day-book/t-form-day-book/t-form-day-book.component.ts
+++ src/app/theme/reports/daily-reports/t-form-day-book/t-form-day-book/t-form-day-book.component.ts
@@ -14,33 +14,33 @@ import { environment } from 'src/environments/environment';
   providers: [OwnbranchMasterService]
 })
 export class TFormDayBookComponent implements OnInit {
- 
-  iframe1url:any='';
+
+  iframe1url: any = '';
   formSubmitted = false;
-   //date
+  //date
   maxDate: Date;
   minDate: Date;
   date: any = null
 
-  showRepo:boolean=false;
- 
-  clicked=false;
+  showRepo: boolean = false;
+
+  clicked = false;
   //Form Group 
   ngForm: FormGroup;
 
   //ng model
-  ngbranch:any = null 
+  ngbranch: any = null
   report_url = environment.report_url;
 
   //Dropdown option variable
 
   branchOption: any[];
-// for Type
+  // for Type
   selectedType
   Types = [
-    { id: 1, value:"Summary" },
-    { id: 2, value:"Details"},
-    { id: 3, value:"Subsidairy Grouping"},
+    { id: 1, value: "Summary" },
+    { id: 2, value: "Details" },
+    { id: 3, value: "Subsidairy Grouping" },
   ];
   constructor(private fb: FormBuilder, private sanitizer: DomSanitizer, public router: Router, private _ownbranchmasterservice: OwnbranchMasterService,) {
     this.maxDate = new Date();
@@ -63,49 +63,49 @@ export class TFormDayBookComponent implements OnInit {
       Branch: ['', [Validators.required]],
       Type: ['', [Validators.required]],
 
-      date:  ["", [ Validators.required]],
-    
+      date: ["", [Validators.required]],
+
 
     });
   }
 
   src: any;
   view(event) {
-    
-    
+
+
     event.preventDefault();
     this.formSubmitted = true;
     let userData = JSON.parse(localStorage.getItem('user'));
     let bankName = userData.branch.syspara.BANK_NAME;
     let branchName = userData.branch.NAME;
-    if(this.ngForm.valid){
-      
-     this.showRepo = true;
-    let obj = this.ngForm.value
-    let Date = moment(obj.date).format('DD/MM/YYYY');
-    
-    let branch = obj.BRANCH_CODE;
-  
-  let type =obj.Type;
-  
-  
-   this.iframe1url=this.report_url+"examples/DayBookfinal1.php?Date='" + Date + "'&branchName='"+branchName+"'&type='"+type +"'&bankName='"+bankName+"' ";
-   this.iframe1url=this.sanitizer.bypassSecurityTrustResourceUrl(this.iframe1url);
+    if (this.ngForm.valid) {
+
+      this.showRepo = true;
+      let obj = this.ngForm.value
+      let Date = moment(obj.date).format('DD/MM/YYYY');
+
+      let branch = obj.BRANCH_CODE;
+
+      let type = obj.Type;
+
+
+      this.iframe1url = this.report_url + "examples/DayBookfinal1.php?Date=" + Date + "&Branch=" + this.ngbranch + "&branchName=" + branchName + "'&type='" + type + "'&bankName='" + bankName + "' ";
+      this.iframe1url = this.sanitizer.bypassSecurityTrustResourceUrl(this.iframe1url);
+    }
+    else {
+      Swal.fire('Warning!', 'Please Fill All Mandatory Field!', 'warning').then(() => { this.clicked = false });
+    }
+
+
   }
-  else {
-    Swal.fire('Warning!', 'Please Fill All Mandatory Field!', 'warning').then(()=>{ this.clicked=false});
+  close() {
+    this.resetForm()
   }
 
-  
+  resetForm() {
+    this.createForm()
+    this.showRepo = false;
+    this.clicked = false;
   }
-  close(){
-    this.resetForm()
-    }
-  
-    resetForm(){
-      this.createForm()
-      this.showRepo = false;
-      this.clicked=false;
-    }
 
 }
