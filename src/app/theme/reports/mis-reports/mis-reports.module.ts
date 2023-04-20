959659d983ce872bd509894d4ac766071ac3ab77
VasimM10 <vasimmangole27@gmail.com>
2023-04-20 15:59:37 +0530

    reports,Locker Register,Excess Cash Balance

---
+++ src/app/theme/reports/mis-reports/mis-reports.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MisReportsRoutingModule } from './mis-reports-routing.module';
+import { ExcessCashBalanceComponent } from './excess-cash-balance/excess-cash-balance.component';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    MisReportsRoutingModule
+  ]
+})
+export class MisReportsModule { }
