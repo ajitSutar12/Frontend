d5eb30e6582f6eed9e77f91597e02d94ce1bed37
Tanuja <tanujapatil8484@gmail.com>
2023-08-31 17:29:35 +0530

    Breadcrump updated changes

--- src/app/theme/master/maintainance/npa-opening-details-entry/npa-opening-details-entry-routing.module.ts
+++ src/app/theme/master/maintainance/npa-opening-details-entry/npa-opening-details-entry-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
     path: '',
     component: NpaOpeningDetailsEntryComponent,
     data: {
-      title: 'NPA Open Details Ent',
+      title: 'Opening Details Entry',
       icon: 'icon-home',
       caption: 'lorem ipsum dolor sit amet, consectetur adipisicing elit',
       status: true
