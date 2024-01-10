c2305c37ee2c61250dd2ba37b1581921f220ed1f
Shubhangi <shubhangi.nagare@compservtech.com>
2024-01-10 14:31:27 +0530

    interest calculation deletion

--- src/app/theme/utility/interest-posting/interest-passing/calculate-interest-deletion/calculate-interest-deletion.module.ts
+++ src/app/theme/utility/interest-posting/interest-passing/calculate-interest-deletion/calculate-interest-deletion.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { CalculateInterestDeletionComponent } from './calculate-interest-deletion.component';
 import { CalculateInterestDeletionRoutingModule } from './calculate-interest-deletion-routing.module';
-import {SharedModule} from '../../../../../shared/shared.module';
-import {DataTablesModule} from 'angular-datatables';
+import { SharedModule } from '../../../../../shared/shared.module';
+import { DataTablesModule } from 'angular-datatables';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { UserAuthInterceptor } from '../../../../../user-auth.interceptor';
+import { EditInterestCalculationService } from '../edit-interest-calculation/edit-interest-calculation.service';
+
 @NgModule({
   imports: [
     CommonModule,
@@ -15,11 +17,11 @@ import { UserAuthInterceptor } from '../../../../../user-auth.interceptor';
     SharedModule,
     DataTablesModule
   ],
-  declarations: [ CalculateInterestDeletionComponent],
-  providers:[{
+  declarations: [CalculateInterestDeletionComponent],
+  providers: [{
     provide: HTTP_INTERCEPTORS,
     useClass: UserAuthInterceptor,
     multi: true
-  },]
+  }, EditInterestCalculationService]
 })
 export class CalculateInterestDeletionModule { }
